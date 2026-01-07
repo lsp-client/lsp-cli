d647b965b6b718f872fc826f72423edd2c11b9f0
observerw <wozluohd@gmail.com>
2026-01-07 17:56:52 +0800

    update

--- src/lsp_cli/client.py
+++ src/lsp_cli/client.py
@@ -5,15 +5,15 @@ from lsp_client.client import Client
 from lsp_client.clients.lang import lang_clients
 
 
-class TargetLspClient(NamedTuple):
+class TargetClient(NamedTuple):
     project_path: Path
     client_cls: type[Client]
 
 
-def find_client(path: Path) -> TargetLspClient | None:
+def find_client(path: Path) -> TargetClient | None:
     candidates = lang_clients.values()
 
     for client_cls in candidates:
         lang_config = client_cls.get_language_config()
         if root := lang_config.find_project_root(path):
-            return TargetLspClient(project_path=root, client_cls=client_cls)
+            return TargetClient(project_path=root, client_cls=client_cls)
